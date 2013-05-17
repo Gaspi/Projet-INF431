4835adcea4dfcd4875bcb4bf560308f6ae8ab60b
Gaspard FEREY <gaspard.ferey@hotmail.fr>
2013-05-17 22:19:44 +0200

    How to use exec functions

--- Java Workspace/Test Hash/src/Main.java
+++ Java Workspace/Test Hash/src/Main.java
@@ -13,9 +13,21 @@ public class Main {
 			
 			if (args[0].compareTo("filtre") == 0)
 				Filter.exec();
-						
+			
 		}
-
+		
 	}
-
+	
+	// The exec functions do the default behavior as described in the subject :
+	// Filter : filter a file from stdin to an adress
+	// HyperLogLog : estimate the number of words in the stdin
+	// hashFunctionTests2 (why "2" ?) : Description of the results of the hashing functions on the stdin
+	// etc (remains to be implemented)
+	//
+	// These functions have to be called in the main function, so that they respond to the command :
+	// java Filter < file  , java HyperLogLog < file , ...
+	//
+	// They can also be called through Main.java with a parameter
+	//
+	// if no parameter is given to Main.java, we could for example provide a more friendly user interface...
 }
